5d8faf59c6e91ba444978b8c131052973df1d7e8
imdhruvgupta <gdhruv167@gmail.com>
2020-05-30 13:07:57 +0530

    Data fetching

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 
 export default function Home() {
-  return (
-    <section class="section">
-      <h1> Listing Sessions </h1>
-      <Link to="/sessions/new">Add a New Session</Link>
-       <table class="table">
+    
+    const [sessions, setSessions] = useState([]); 
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    useEffect(() => {
+        fetch(`http://localhost:3000/sessions`, options)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setSessions(result);
+        })
+    }, [])
+    
+    return (
+        <section className="section">
+        <h1> Listing Sessions </h1>
+        <Link to="/sessions/new">Add a New Session</Link>
+        <table className="table">
         <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Interviewee Id</th>
-            <th scope="col">Start Time</th>
-            <th scope="col">End Time</th>
-            <th scopt="col"></th>
-          </tr>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Interviewee Id</th>
+        <th scope="col">Start Time</th>
+        <th scope="col">End Time</th>
+        <th scopt="col"></th>
+        </tr>
         </thead>
         <tbody>
-          <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>24/5/2020</td>
-          <td>25/5/2020</td>
-          <td>
-            <Link to={`/sessions/${1}/edit`}>Edit</Link>
-          </td>
-          </tr>
+        {sessions.map(session => (
+            <tr key={session.id}>
+            <td>{session.id}</td>
+            <td>{session.user_id}</td>
+            <td>{session.start}</td>
+            <td>{session.end}</td>
+            <td>
+            <Link to={`/sessions/${session.id}/edit`}>Edit</Link>
+            </td>
+            </tr>
+        ))}
         </tbody>
-       </table>
-   </section>
-  );
-}
+        </table>
+        </section>
+        );
+    }
--- src/views/New.js
+++ src/views/New.js
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 
 export default function New() {
+    let history = useHistory();
+
+    const [start, setStart] = useState();
+    const [end, setEnd] = useState();
+    const [userId, setUserId] = useState('');
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch(`http://localhost:3000/users/`, options)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setUsers(result);
+        })
+    }, []);
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            body: new URLSearchParams({
+                "session[start]": start,
+                "session[end]": end,
+                "session[user_id]": userId
+            })
+        };
+        fetch(`http://localhost:3000/sessions/`, options)
+        .then(res => res.json())
+        .then(
+          (result) => {
+           console.log(result)
+           history.push('/')
+        })
+    }
+
     return (
-      <section class="section">
-        <h1> Add a New Sessions </h1>
+      <section className="section">
+        <h1> Add a New Session </h1>
         <form>
-          <div class="form-row">
-            <div class="form-group col-6">
-              <label for="exampleFormControlInput1">Start Time</label>
-              <input type="datetime-local" id="start" name="start" />
+          <div className="form-row">
+            <div className="form-group col-6">
+              <label>Start Time</label>
+              <input type="datetime-local" id="start" name="start" onChange={e => setStart(e.target.value)} />
             </div>
-            <div class="form-group col-6">
-              <label for="exampleFormControlInput1">End Time</label>
-              <input type="datetime-local" id="end" name="end" />
+            <div className="form-group col-6">
+              <label>End Time</label>
+              <input type="datetime-local" id="end" name="end" onChange={e => setEnd(e.target.value)}/>
             </div>
+            <select className="form-control col-8" onChange={e => setUserId(e.target.value)}>
+                {users.map((user) => (
+                    <option key={user.id} value={user.id}>{user.username}</option>
+                ))}
+            </select>
           </div>
-          <button id="submit" class="btn btn-primary">Submit</button>
+          <button id="submit" className="btn btn-primary mt-3" onClick={handleSubmit}>Submit</button>
         </form>
       </section>
     );
